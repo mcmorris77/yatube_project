a6246b661e4aba12cf7a84879d799e735cab1828
Simon Mcmorris <2007saltan@gmail.com>
2026-03-05 12:40:34 +0300

    Написан test_urls, но не проверен, внизу написано, что надо доделать

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('create/', views.post_create, name='post_create'),
     path('posts/<int:pk>/edit', views.post_edit, name='post_edit'),
     path('auth/', include('django.contrib.auth.urls'))
+
 ]
